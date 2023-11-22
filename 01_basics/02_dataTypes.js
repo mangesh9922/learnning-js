48a0e0eb7b8ba17264da6f0a06e40089d2262faa
mangesh9922 <93697788+mangesh9922@users.noreply.github.com>
2023-11-22 20:19:51 +0000

    new commit

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,42 @@
+"use strict"; // treat all javascript code as new js
+
+// alert(3 + 3) // we are using nodejs not browser
+
+
+/*
+Primitive Data Types:
+
+Number: Represents both integer and floating-point numbers. Example: let num = 42;
+
+String: Represents sequences of characters. Example: let str = "Hello, World!";
+
+Boolean: Represents either true or false. Example: let isTrue = true;
+
+Null: Represents the intentional absence of any object value. Example: let nullValue = null;
+
+Undefined: Represents an uninitialized variable or missing function arguments. Example: let undefinedValue;
+
+BigInt: Represents integers of arbitrary precision. Example: let bigIntNum = 9007199254740991n;
+*/
+
+/* Non-Primitive Data Types:
+
+JavaScript has a non-primitive data type called Object, which includes functions, arrays, and other complex structures.
+*/
+
+/*
+let person = {
+  name: "John",
+  age: 30,
+  isStudent: false
+};
+
+*/
+let num=34 //number
+let name="mangesh" //string
+let islogged= false //boolean
+let nullValue=null //null (acts as stand alone value)
+let whether; // undefined value
+
+console.log(typeof null); // object
+console.log(typeof undefined) // undefined
