7d6752bd6129e540070a9e427b19e5213d950a17
mangesh9922 <93697788+mangesh9922@users.noreply.github.com>
2023-12-27 18:39:50 +0000

    functions

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,24 @@
+function Myname() {
+    console.log("mangesh gajakosh");
+}
+
+Myname();
+
+function addTwoNum(number1,number2){//parameters (number1,number2)
+    console.log(number1 + number2);
+}
+
+addTwoNum(3,4); // arguments(3,4)
+
+// function LoginUserMessage(username){
+//     return `${username} just logged in`
+// }
+
+// console.log(LoginUserMessage("mangesh"));
+
+function LoginUserMessage(username){
+    return `${username} just logged in`
+}
+
+console.log(LoginUserMessage())
+
